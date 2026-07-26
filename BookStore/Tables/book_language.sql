e6eb236179b1f3585c73447b464d2787bacaa92b
jbqui <jbqui@AZ-BRXBNQ3>
2026-07-26 08:41:14 -0400

    Add project files.

---
+++ BookStore/Tables/book_language.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[book_language]
+(
+	[language_id] INT constraint PK_BOOK_LANGUAGE PRIMARY KEY,
+	[language_code] varchar(10),
+	[language_name] varchar(50),
+	[rowversion] rowversion
+)
---
+++ BookStore/Tables/publisher.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[publisher]
+(
+	[publisher_id] INT constraint PK_PUBLISHER PRIMARY KEY,
+	[publisher_name] varchar(50),
+	[rowversion] rowversion
+
+)
