076f6d03ed21b27c4ce770cf58b6de3f444c22cc
aearanao-svg <aearanao@soe.uagrm.edu.bo>
2026-07-26 22:00:40 -0400

    Corrige validación de páginas en tabla book
    Se corrigió el CHECK de num_pages para permitir únicamente
    cantidades mayores a cero y se cambió el nombre de la
    restricción CK_ESTUDIANTE por CK_BOOK_NUM_PAGES.

--- BookStore/Tables/book.sql
+++ BookStore/Tables/book.sql
@@ -6,7 +6,7 @@ CREATE TABLE [dbo].[book]
 	[title] varchar(50) Not Null ,
 	[isbn13] varchar(20) UNIQUE,
 	[language_id] int CONSTRAINT FK_BOOK_LANGUAGE Foreign Key(language_id) References dbo.book_language(language_id),
-	[num_pages] int COnstraint CK_ESTUDIANTE check(num_pages<0),
+	[num_pages] INT CONSTRAINT CK_BOOK_NUM_PAGES CHECK(num_pages > 0),
 	[publication_date] date,
 	[publisher_id] int CONSTRAINT FK_BOOK_PUBLISHER Foreign key(publisher_id) References dbo.publisher(publisher_id),
 	[rowversion] rowversion
