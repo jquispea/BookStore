920d7e1aaa03918fcac050bf603ad97796c4cb4f
billy <billy@LAPTOP-6HU8HQB0>
2026-08-14 20:12:17 -0400

    ADD: ETL dimCustomer

---
+++ BookStoresDW/Schema/Programmability/Stored Procedures/DW_MergeDimCustomer.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[DW_MergeDimCustomer]
+AS
+BEGIN
+
+    UPDATE dc
+        SET FirstName = sc.FirstName,
+            LastName  = sc.LastName,
+            Email     = sc.Email,
+            Country   = sc.CountryName
+    FROM dbo.DimCustomer dc
+    INNER JOIN staging.StgCustomer sc
+        ON dc.CustomerID = sc.CustomerID;
+
+END
+GO
