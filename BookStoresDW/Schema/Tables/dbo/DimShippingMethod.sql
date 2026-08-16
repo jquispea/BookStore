b951824057f3278d9807092d7858f2e003a8eff6
jbqui <jbquispea@soe.uagrm.edu.bo>
2026-08-16 17:48:02 -0400

    adding DinShippingM

---
+++ BookStoresDW/Schema/Tables/dbo/DimShippingMethod.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE DimShippingMethod
+(
+    ShippingMethodKey INT IDENTITY(1,1) NOT NULL,
+    method_id INT NOT NULL,
+    method_name VARCHAR(100) NOT NULL,
+    cost DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT PK_DimShippingMethod
+        PRIMARY KEY (ShippingMethodKey)
+);
